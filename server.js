998efc798bfbbf8b635a40ceb052e5efab2a347e
Kelsey <kelsgainer@gmail.com>
2019-03-23 13:52:40 -0600

    server update

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+let db = require("./models");
 // Define API routes here
 
 // Send every other request to the React app
@@ -19,6 +20,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+db.sequelize.sync({ force: true }).then(function() {
+	app.listen(PORT, () => {
+  		console.log(`🌎 ==> API server now on port ${PORT}!`);
+	});
 });
