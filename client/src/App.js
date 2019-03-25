812bf1c3693c61266e886fab29fc540653e62111
Drakeen <info@howardfms.com>
2019-03-25 17:39:25 -0600

    auth0 and db updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, withRouter} from 'react-router-dom';
+import auth0Client from './Auth';
+import NavBar from './NavBar/NavBar';
+import Question from './Question/Question';
+import Questions from './Questions/Questions';
+import Callback from './Callback';
+import NewQuestion from './NewQuestion/NewQuestion';
+import SecuredRoute from './SecuredRoute/SecuredRoute';
 
 class App extends Component {
+  async componentDidMount() {
+  if (this.props.location.pathname === '/callback') return;
+  try {
+    await auth0Client.silentAuth();
+    this.forceUpdate();
+  } catch (err) {
+    if (err.error !== 'login_required') console.log(err.error);
+  }
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <NavBar></NavBar>
+        <Route exact path='/' component={Questions}/>
+        <Route exact path='/question/:questionId' component={Question}/>
+        <Route exact path='/callback' component={Callback}/>
+        <SecuredRoute path='/new-question' component={NewQuestion} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- server.js
+++ server.js
@@ -1,24 +1,107 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// *****************
+// the database
+const questions = [];
+
+// enhance your app security with Helmet
+app.use(helmet());
+
+// use bodyParser to parse application/json content-type
+app.use(bodyParser.json());
+
+// enable all CORS requests
+app.use(cors());
+
+// log HTTP requests
+app.use(morgan('combined'));
+// **********
+
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 let db = require("./models");
 // Define API routes here
 
+//  ************
+// get a specific question
+app.get('/:id', (req, res) => {
+  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+  res.send(question[0]);
+});
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://desire2expire.auth0.com/.well-known/jwks.json'
+  }),
+
+  //validate the audience and the issuer
+  audience: 'WgymmhrW4XsS23FurdObVghA11C0ghbQ',
+  issuer: `https://desire2expire.auth0.com/`,
+  algorithms: ['RS256']
+});
+
+// insert a new question
+app.post('/', checkJwt, (req, res) => {
+  const {title, description} = req.body;
+  const newQuestion = {
+    id: questions.length + 1,
+    title,
+    description,
+    answers: [],
+    author: req.user.name,
+  };
+  questions.push(newQuestion);
+  res.status(200).send();
+});
+
+// insert a new answer to a question
+app.post('/answer/:id', checkJwt, (req, res) => {
+  const {answer} = req.body;
+
+  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+
+  question[0].answers.push({
+    answer,
+    author: req.user.name,
+  });
+
+  res.status(200).send();
+});
+
+// start the server
+// app.listen(8081, () => {
+//   console.log('listening on port 8081');
+// });
+// *************
+
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 db.sequelize.sync({ force: true }).then(function() {
 	app.listen(PORT, () => {
