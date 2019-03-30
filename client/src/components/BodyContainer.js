46f3e2cd2b1fea29a882ddaee1216e75b62da7ad
Drakeen <info@howardfms.com>
2019-03-30 11:25:51 -0600

    BodyContainer component and style

--- client/src/components/BodyContainer.js
+++ client/src/components/BodyContainer.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const BodyContainer = (props) => {
     return (
-        <div className="bodyContainer">
-            <h1>Body content goes here: {props.myText}</h1>
+        <div className="bodyContainer container sm-col-5">
+            <div className="bodyContent container">
+                <br></br>
+                <h1 className="mt-3">Body content goes here: {props.myText}</h1>
+            </div>
         </div>
     )
 }
