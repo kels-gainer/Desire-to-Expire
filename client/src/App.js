ae51893202138696b07a806833631a165ec50b96
TimothyLee504 <timothylee504@yahoo.com>
2019-04-01 19:40:27 -0600

    modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
+import Modal from './components/Signup'
+
 import logo from './logo.svg';
 import './App.css';
+import FormContainer from './components/Container';
 
 class App extends Component {
+
+      constructor() {
+        super();
+
+        this.state = {
+            isShowing: false
+        }
+    }
+
+    openModalHandler = () => {
+        this.setState({
+            isShowing: true
+        });
+    }
+
+    closeModalHandler = () => {
+        this.setState({
+            isShowing: false
+        });
+    }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="callout-basics-example">
+          <div className="row">
+            <div className="column">
+                { this.state.isShowing ? <div onClick={this.closeModalHandler} className="back-drop"></div> : null }
+                <button className="open-modal-btn" style={{zIndex: 100}} onClick={this.openModalHandler}>Add Food</button>
+                </div>
+            <div className="column">
+                <Modal
+                    className="modal"
+                    show={this.state.isShowing}
+                    close={this.closeModalHandler}>
+                    {/* <FormContainer/> */}
+                </Modal>
+              </div>
+            </div>
+          </div>
         </header>
       </div>
     );
   }
-}
+} 
+
 
 export default App;
